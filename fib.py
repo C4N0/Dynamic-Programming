05983e032c1fa14d1ea4c1b97cc58eae58dfb641
C4N0 <76574048+C4N0@users.noreply.github.com>
2021-12-30 01:47:22 +0100

    Add files via upload

---
+++ fib.py
@@ -0,0 +1,25 @@
+import time
+
+
+def fib40(n):
+    if n <= 1:
+        return n
+    else:
+        return fib40(n-1) + fib40(n-2)
+
+
+def fib_memo(n, memo):
+    # Memoisierung zur Reduzierung der 2**n Komplexität
+    if n in memo:
+        return memo[n]
+    elif n <= 1:
+        return n
+    else:
+        memo[n] = fib_memo(n-1, memo) + fib_memo(n-2, memo)
+        return memo[n]
+start = time.time()
+print(fib_memo(40, {}))
+end = time.time()
+
+exact_time = end - start
+print(exact_time)
---
+++ gridtraveler.py
@@ -0,0 +1,41 @@
+"""Number of possibilities to travel through a mxn matrix with just
+    the "up" and "right" arrow keys"""
+import time
+import pandas as pd
+
+
+def gridTravel(m, n):
+    if m == 1 and n == 1:  # Base case for recursion
+        return 1
+    elif m == 0 or n == 0:  # another base case
+        return 0
+    else:
+        return gridTravel(m - 1, n) + gridTravel(m, n - 1)
+
+
+def gridTravel_memo(m, n, memo):
+    key = str(m) + "," + str(n)  # delineates a cacheable variable for the memo
+    if key in memo:
+        return memo[key]
+    if m == 1 and n == 1:
+        return 1
+    if m == 0 or n == 0:
+        return 0
+    else:
+        memo[key] = gridTravel_memo(m - 1, n, memo) + gridTravel_memo(m, n - 1, memo)
+        return memo[key]
+
+
+start = time.time()
+a = gridTravel_memo(10, 10, {})
+b = gridTravel_memo(20, 20, {})
+c = gridTravel_memo(30, 30, {})
+d = gridTravel_memo(40, 40, {})
+
+end = time.time()
+
+exact_time = end - start  # time in seconds needed to run the code
+print(exact_time)
+
+frame = pd.DataFrame({"10x10 Matrix": a, "20x20 Matrix": b, "30x30 Matrix": c, "40x40 Matrix": d},{"Possibilities"})
+print(frame)
